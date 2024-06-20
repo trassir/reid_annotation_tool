7dff41ba21e5cd937fe56cebb44a5a84aee74fb9
artembazeev <abwap3@gmail.com>
2024-06-20 17:26:48 +0400

    add models

--- reid_annotation_tool/reid/models.py
+++ reid_annotation_tool/reid/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from pgvector.django import VectorField
 
-# Create your models here.
+
+class Person(models.Model):
+    id = models.CharField(
+        max_length=6, primary_key=True, unique=True, verbose_name="Person ID"
+    )
+    crop_ids = models.JSONField(default=list, verbose_name="Crops list")
+
+    def __str__(self):
+        return self.id
+
+
+class Crop(models.Model):
+    id = models.CharField(
+        max_length=255, primary_key=True, unique=True, verbose_name="Crop ID"
+    )
+    embedding = VectorField(dimensions=1024, verbose_name="Embedding")
+
+    def __str__(self):
+        return self.id
